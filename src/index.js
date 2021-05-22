326683e5c2aec630389efd22ebe9f40c6690dd37
Mariia-Kovalchuk <72386697+Mariia-Kovalchuk@users.noreply.github.com>
2021-05-22 03:30:03 +0300

    IntersectionObserver

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import './sass/main.scss';
+import { error } from "@pnotify/core";
+import "@pnotify/core/dist/PNotify.css";
+import "@pnotify/core/dist/BrightTheme.css";
+import "@pnotify/confirm/dist/PNotifyConfirm.css";
 
 import ApiService from './js/apiService';
 import imageCardTpl from './templates/image-card.hbs';
+
 
 const refs = {
     imageQuery: document.querySelector('.search-form'),
@@ -16,9 +21,67 @@ refs.imageQuery.addEventListener('input', debounce(onSearch, 1000));
 
 function onSearch(e) {
     refs.imageGallery.innerHTML = ' ';
+    apiSearchService.page = 1;
+
     apiSearchService.query = e.target.value;
-    console.dir(apiSearchService);
+    if (apiSearchService.searchQuery.length > 0) {
+        apiSearchService.resetPage();
+        apiSearchService.search()
+            .then(images => {
+                if (images.length) {
+                    refs.imageGallery.insertAdjacentHTML('beforeend', imageCardTpl(images));
+                    console.log(document.querySelector('li:last-child'));
+                    observer.observe(document.querySelector('li:last-child'));
+                    
+                } else {
+                    errorQuery(e);
+                };
+            })
+            .catch(console.log);
+        
+    };
+};
+
+function errorQuery(e) {
+    e.target.value = '';
+    error({
+        text: "No match found. Please check your query!",
+        delay: 2000,
+    });
+};
+
+
+// функция создания элемента списка
+function loadList(){
+    apiSearchService.incrementPage();
+    console.log(apiSearchService.page);
     apiSearchService.search().then(images => {
         refs.imageGallery.insertAdjacentHTML('beforeend', imageCardTpl(images))
     });
+
 };
+
+
+// для того, чтобы все время наблюдать за последним элементом списка
+// мы используем нечто вроде замыкания
+// прекращаем наблюдать за целевым элементом после создания очередного li
+// и начинаем наблюдать за этим новым (последним) элементом
+var observer = new IntersectionObserver((entries, observer) => {
+    // console.dir(entries);
+    entries.forEach(entry => {
+        // console.log(entry.isIntersecting);
+        if (entry.isIntersecting) {
+            console.log("entry.isIntersecting");
+            loadList()
+        }
+        // console.log(entry.target);
+        observer.unobserve(entry.target)
+
+        // console.log(document.querySelector('li:last-child'));
+        observer.observe(document.querySelector('li:last-child'));
+    });
+        // console.dir(entries);
+
+}, {
+    threshold: 1
+});
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -8,7 +8,8 @@ export default class ApiService {
     this.page = 1;
     }
 
-    search() {     
+    search() {
+        
         return fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${API_KEY}`)
             .then(response => response.json())
             .then(({ hits }) => {
@@ -21,9 +22,17 @@ export default class ApiService {
         this.searchQuery = newQuery;
     }
 
+
     // get query() {
     //     return this.page;
     // }
+    incrementPage() {
+        this.page+=1;
+    }
+
+    resetPage() {
+    this.page = 1;
+  }
 
 
     
